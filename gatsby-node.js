36cf32829a2a5ff929be2a857db5c140830c70db
Bradley Lewis <blewie144@gmail.com>
2020-10-22 10:23:45 +1030

    added individual pages for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+// Preparing request to contentful
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+    resolve(
+        graphql(request).then(result => {
+            if (result.errors) {
+                reject(result.errors)
+            }
+            return result;
+        })
+    )
+});
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions;
+
+    // Create pages for each blog, with data from contentful
+    const getBlog = makeRequest(graphql, `
+        {
+        allContentfulBlog (
+            sort: { fields: [createdAt], order: DESC }
+            filter: {
+                node_locale: {eq: "en-US"}},)
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        result.data.allContentfulBlog.edges.forEach(({ node }) => {
+            createPage({
+                // Give each blog post its own url 
+                path: `blog/${node.slug}`,
+                // Create each page using this template file
+                component: path.resolve(`src/templates/blog.js`),
+                context: {
+                    id: node.id,
+                },
+            })
+        })
+    });
+
+    return Promise.all([
+        getBlog
+    ])
+};
